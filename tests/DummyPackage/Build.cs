bb6a820e8bb6165eee280b6a1d3fa13ea53f9829
Oskar Klintrot <oskar.klintrot@gmail.com>
2022-02-25 13:30:32 +0100

    chore: Add project to push dummy packages to feed

---
+++ tests/DummyPackage/Build.cs
@@ -0,0 +1,66 @@
+﻿using System.Reflection;
+using Bullseye;
+using McMaster.Extensions.CommandLineUtils;
+using NuGet.Versioning;
+using static Bullseye.Targets;
+using static SimpleExec.Command;
+
+using var app = new CommandLineApplication() { UsePagerForHelpText = false };
+app.HelpOption();
+var version = app.Option<string>("-v|--version <version>", "Version of the package.", CommandOptionType.SingleValue);
+var nameOption = app.Option<string>("-n|--name <name>", "Name of the package.", CommandOptionType.SingleValue);
+
+// translate from Bullseye to McMaster.Extensions.CommandLineUtils
+app.Argument("targets", "A list of targets to run or list. If not specified, the \"default\" target will be run, or all targets will be listed.", true);
+foreach (var (aliases, description) in Options.Definitions)
+{
+    _ = app.Option(string.Join("|", aliases), description, CommandOptionType.NoValue);
+}
+
+app.OnExecuteAsync(async _ =>
+{
+     var msbuild = Assembly
+        .GetEntryAssembly()!
+        .GetCustomAttribute<MsBuildConfigurationAttribute>()!;
+
+    // translate from McMaster.Extensions.CommandLineUtils to Bullseye
+    var targets = app.Arguments[0].Values.OfType<string>();
+    var options = new Options(Options.Definitions.Select(d => (d.Aliases[0], app.Options.Single(o => d.Aliases.Contains($"--{o.LongName}")).HasValue())));
+
+    var nuGetVersion = NuGetVersion.Parse(version.Value());
+    var packageId = nameOption.Value();
+
+    Target("default", async () =>
+    {
+        await Console.Out.WriteLineAsync($"version:   {nuGetVersion}");
+        await Console.Out.WriteLineAsync($"packageId: {packageId}");
+
+        await RunAsync(
+            "dotnet",
+            $"pack --configuration Release -p:version=\"{nuGetVersion}\" -p:packageId=\"{packageId}\" {msbuild.ProjectDir} --output ."
+        );
+    });
+
+    Target("push", DependsOn("default"), async () =>
+    {
+        await RunAsync(
+            "dotnet",
+            $"nuget push .\\{packageId}.{nuGetVersion}.nupkg --api-key AzureDevOps --source https://pkgs.dev.azure.com/curium/0329093f-bb7c-41c0-9e4c-893f37483900/_packaging/broken-parts/nuget/v3/index.json --interactive"
+        );
+    });
+
+    await RunTargetsAndExitAsync(targets, options);
+});
+
+return await app.ExecuteAsync(args);
+
+[AttributeUsage(AttributeTargets.Assembly, Inherited = false, AllowMultiple = false)]
+sealed class MsBuildConfigurationAttribute : Attribute
+{
+    public string ProjectDir { get; }
+
+    public MsBuildConfigurationAttribute(string projectDir)
+    {
+        ProjectDir = projectDir;
+    }
+}
