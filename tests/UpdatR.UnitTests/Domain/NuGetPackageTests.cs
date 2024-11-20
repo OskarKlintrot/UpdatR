871b6ae57505433f3f5b8842e09287451901ee41
Oskar Klintrot <oskar@pbgbg.com>
2024-11-20 13:22:17 +0100

    feat: Ignore .NETStandard when other targets exists

--- tests/UpdatR.UnitTests/Domain/NuGetPackageTests.cs
+++ tests/UpdatR.UnitTests/Domain/NuGetPackageTests.cs
@@ -68,4 +68,120 @@ public class NuGetPackageTests
             Assert.Equal(expectedNewResult, packageMetadata?.Version.ToString());
         }
     }
+
+    [Theory]
+    [InlineData("net6.0", "2.0.0")]
+    [InlineData("net7.0", "2.0.0")]
+    [InlineData("net9.0", "2.0.0")]
+    [InlineData("netstandard2.0", "2.0.0")]
+    [System.Diagnostics.CodeAnalysis.SuppressMessage(
+        "Naming",
+        "CA1707:Identifiers should not contain underscores",
+        Justification = "Test name"
+    )]
+    public void TryGetLatestComparedTo_NetStandard_OnlySupport(string tfm, string? expected = null)
+    {
+        // Arrange
+        var package = new NuGetPackage(
+            "package-id",
+            [
+                new PackageMetadata(
+                    NuGetVersion.Parse("1.0.0"),
+                    [NuGetFramework.Parse("netstandard2.0")],
+                    null,
+                    null
+                ),
+                new PackageMetadata(
+                    NuGetVersion.Parse("2.0.0"),
+                    [NuGetFramework.Parse("netstandard2.0")],
+                    null,
+                    null
+                ),
+            ]
+        );
+
+        // Act
+        var newerVersionIsAvailable = package.TryGetLatestComparedTo(
+            version: NuGetVersion.Parse("1.0.0"),
+            targetFramework: NuGetFramework.Parse(tfm),
+            usePrerelease: false,
+            package: out var packageMetadata
+        );
+
+        // Assert
+        if (expected is null)
+        {
+            Assert.False(newerVersionIsAvailable);
+        }
+        else
+        {
+            Assert.True(newerVersionIsAvailable);
+            Assert.Equal(expected, packageMetadata?.Version.ToString());
+        }
+    }
+
+    [Theory]
+    [InlineData("net6.0")]
+    [InlineData("net7.0", "2.0.0")]
+    [InlineData("net9.0", "3.0.0")]
+    [InlineData("netstandard2.0", "3.0.0")]
+    [System.Diagnostics.CodeAnalysis.SuppressMessage(
+        "Naming",
+        "CA1707:Identifiers should not contain underscores",
+        Justification = "Test name"
+    )]
+    public void TryGetLatestComparedTo_Ignore_NetStandard(string tfm, string? expected = null)
+    {
+        // Arrange
+        var package = new NuGetPackage(
+            "package-id",
+            [
+                new PackageMetadata(
+                    NuGetVersion.Parse("1.0.0"),
+                    [
+                        NuGetFramework.Parse("netstandard2.0"),
+                        NuGetFramework.Parse("net6.0"),
+                        NuGetFramework.Parse("net7.0"),
+                    ],
+                    null,
+                    null
+                ),
+                new PackageMetadata(
+                    NuGetVersion.Parse("2.0.0"),
+                    [
+                        NuGetFramework.Parse("netstandard2.0"),
+                        NuGetFramework.Parse("net7.0"),
+                        NuGetFramework.Parse("net8.0"),
+                    ],
+                    null,
+                    null
+                ),
+                new PackageMetadata(
+                    NuGetVersion.Parse("3.0.0"),
+                    [NuGetFramework.Parse("netstandard2.0"), NuGetFramework.Parse("net9.0")],
+                    null,
+                    null
+                ),
+            ]
+        );
+
+        // Act
+        var newerVersionIsAvailable = package.TryGetLatestComparedTo(
+            version: NuGetVersion.Parse("1.0.0"),
+            targetFramework: NuGetFramework.Parse(tfm),
+            usePrerelease: false,
+            package: out var packageMetadata
+        );
+
+        // Assert
+        if (expected is null)
+        {
+            Assert.False(newerVersionIsAvailable);
+        }
+        else
+        {
+            Assert.True(newerVersionIsAvailable);
+            Assert.Equal(expected, packageMetadata?.Version.ToString());
+        }
+    }
 }
